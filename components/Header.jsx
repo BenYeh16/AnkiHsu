a2816fad2651a2e6eee2b8c67f1bd82438ea57d9
軒 <b02902016@ntu.edu.tw>
2017-07-09 22:04:56 +0800

    facebook instagram mouseOnClick

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,52 +1,72 @@
 import React from 'react';
 
-export default function Header() {
-    return (
-        <div style={{
-            position: 'relative',
-            width: '100%',
-            height: '293.5px'
-        }}>
-           <img
-                src="https://ankihsu.com/wp-content/uploads/2017/07/LOGO.png"
-                style={{
-                    width: '69px',
-                    height: '69px',
-                    position: 'absolute',
-                    transform: 'translateX(-50%)',
-                    left: '50%',
-                    top: '45px'
-                }}
-            />
-            <img
-                src="https://ankihsu.com/wp-content/uploads/2017/07/Anki-Hsu-Makeup-Ha.png"
-                style={{
-                    position: 'absolute',
-                    transform: 'translateX(-50%)',
-                    left: '50%',
-                    top: '134px'                
-                }}
-            />
-            <img 
-                src="https://ankihsu.com/wp-content/uploads/2017/07/FB.png"
-                style={{
-                    width: '35px',
-                    height: '35px',
-                    position: 'absolute',
-                    right: '135px',
-                    top: '134px'
-                }}
-            />
-            <img 
-                src="https://ankihsu.com/wp-content/uploads/2017/07/instagram.png"
-                style={{
-                    width: '35px',
-                    height: '35px',
-                    position: 'absolute',
-                    right: '86.8px',
-                    top: '134px'
-                }}
-            /> 
-        </div>
-    );
+class Header extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            facebookOnHover: false,
+            instagramOnHover: false
+        };
+    }
+
+    render() {
+        const {
+            facebookOnHover,
+            instagramOnHover
+        } = this.state;
+        return (
+            <div style={{
+                position: 'relative',
+                width: '100%',
+                height: '293.5px'
+            }}>
+                <img
+                    src="https://ankihsu.com/wp-content/uploads/2017/07/LOGO.png"
+                    style={{
+                        width: '69px',
+                        height: '69px',
+                        position: 'absolute',
+                        transform: 'translateX(-50%)',
+                        left: '50%',
+                        top: '45px'
+                    }}
+                />
+                <img
+                    src="https://ankihsu.com/wp-content/uploads/2017/07/Anki-Hsu-Makeup-Ha.png"
+                    style={{
+                        position: 'absolute',
+                        transform: 'translateX(-50%)',
+                        left: '50%',
+                        top: '134px'                
+                    }}
+                />
+                <img 
+                    src={ facebookOnHover ? 'https://ankihsu.com/wp-content/uploads/2017/07/FBhovered.png' : 'https://ankihsu.com/wp-content/uploads/2017/07/FB.png'  }
+                    style={{
+                        width: '35px',
+                        height: '35px',
+                        position: 'absolute',
+                        right: '135px',
+                        top: '134px'
+                    }}
+                    onMouseEnter={() => {this.setState({ facebookOnHover: true })}}
+                    onMouseLeave={() => {this.setState({ facebookOnHover: false })}}
+                />
+                <img 
+                    src={ instagramOnHover ? 'https://ankihsu.com/wp-content/uploads/2017/07/instagramhovered.png' : 'https://ankihsu.com/wp-content/uploads/2017/07/instagram.png' }
+                    style={{
+                        width: '35px',
+                        height: '35px',
+                        position: 'absolute',
+                        right: '86.8px',
+                        top: '134px'
+                    }}
+                    onMouseEnter={() => {this.setState({ instagramOnHover: true })}}
+                    onMouseLeave={() => {this.setState({ instagramOnHover: false })}}
+                /> 
+            </div>
+        );
+    }
 };
+
+export default Header;
---
+++ components/Menu.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header.jsx';
+
+class MainPage extends React.Component {
+    render() {
+
+        return (
+            <div>
+                <Header />
+            </div>
+        );
+    }
+};
