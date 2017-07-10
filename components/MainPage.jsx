1daba2f8f66c2d2738b8ce250f39b6cc9741c75e
BenYeh16 <b02902016@ntu.edu.tw>
2017-07-10 19:26:28 +0800

    add SectionDivider.jsx

--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header.jsx';
 import Menu from './Menu.jsx';
+import SectionDivider from './SectionDivider.jsx';
 
 export default function MainPage() {
     return (
         <div className="Bgi(headerbackground) Bgpx(50%) Bgpy(0) Bgz(54%) Bgr(nr)">
             <Header />
             <Menu />
+            <SectionDivider />
         </div>
     );
 };
---
+++ components/SectionDivider.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function SectionDivider(props) {
+    return (
+        <div className="W(100%) H(30px)">
+        </div>
+    );
+};
