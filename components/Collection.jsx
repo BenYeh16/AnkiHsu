52e1e782f37b308af960bcbdd9f911993974f8bd
軒 <b02902016@ntu.edu.tw>
2017-07-12 01:22:36 +0800

    remove empty div

--- components/Collection.jsx
+++ components/Collection.jsx
@@ -10,10 +10,8 @@ export default function Collection() {
         <div className="Bgi(headerbackground) Bgpx(50%) Bgpy(0) Bgz(54%) Bgr(nr) Maw(1440px) Miw(1024px) Mx(a)">
             <Header />
             <Menu />
-            <div className="W(100%) T(21.5px)">
-                <Sidebar />
-                <CollectionBody />
-            </div>
+            <Sidebar />
+            <CollectionBody />
             <Footer />
         </div>
     )
